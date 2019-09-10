6a6317ab6910d1c74e8ab7ba16a74911ed2d38cd
Jason Walton <jwalton@benbria.ca>
2019-09-10 16:05:43 -0400

    test

--- .github/actions/findPr/main.js
+++ .github/actions/findPr/main.js
@@ -3,16 +3,24 @@ const { GitHub, context } = require('@actions/github');
 
 async function main() {
     const token = core.getInput('github-token', {required: true})
-    const sha = core.getInput('sha') || context.sha;
+    const sha = core.getInput('sha');
 
-    const client = new GitHub(token, {});
-    const result = await client.repos.listPullRequestsAssociatedWithCommit({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        commit_sha: sha
-    });
+    let pr;
+    if (!sha && context.eventName === 'pull_request') {
+        pr = context.payload.pull_request.number;
+    } else {
+        // If this is not a pull_request event, there might still be a PR associated with the SHA.
+        const client = new GitHub(token, {});
+        const result = await client.repos.listPullRequestsAssociatedWithCommit({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            commit_sha: sha || context.sha
+        });
 
-    core.setOutput("pr", `${result.data.length > 0 ? result.data[0].number : ''}`);
+        pr = result.data.length > 0 && result.data[0].number;
+    }
+
+    core.setOutput("pr", pr || '');
 }
 
 main().catch(err => core.setFailed(err.message));
